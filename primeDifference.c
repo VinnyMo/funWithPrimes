ec963604404f2f05df64f19e1cc81ff1c7559fb3
Vincent Mossman <mossmanv@student-cs.ad.uni.edu>
2016-09-26 23:34:23 -0500

    Worked on Lucifer Prime Series

--- primeDifference.c
+++ primeDifference.c
@@ -20,6 +20,14 @@ int main(int argc, char * argv[]) {
   int COUNT_TO, i, max = 0;
   sscanf(argv[1], "%d", &COUNT_TO);
   int track[3] = {1, 0, 0}; //Previous Prime Position, Total Distance, Total Primes
+ 
+  int triSeries[2] = {0, 0};
+  int luciferCounter = 0, othersCounter = 0, j, lastUnique = 1;
+  int findUnique[120]; //max difference under 1 mil is 114
+
+  for (i = 0; j < 120; i++) {
+    findUnique[i] = 0;
+  }
 
   clock_t begin = clock();
   printf("\n");
@@ -30,6 +38,31 @@ int main(int argc, char * argv[]) {
       if (i - track[0] > max) {
 	max = i - track[0];
       }
+
+
+      //triSeries test
+      if (triSeries[1] == i - track[0]) {
+	if (findUnique[lastUnique - 1] < i - track[0]) {
+	  printf("{%d %d %d} \n", i - track[0], i - track[0], i - track[0]);
+	  findUnique[lastUnique] = i - track[0];
+	  lastUnique++;
+	}
+	if (i - track[0] == 6) {
+	  luciferCounter++;
+	}
+	else {
+	  othersCounter++;
+	}
+	triSeries[0] = 0;
+	triSeries[1] = 0;
+      }
+      else if (triSeries[0] == i - track[0]) {
+	  triSeries[1] = i - track[0];
+	}
+	else {
+	  triSeries[0] = i - track[0];
+	  }
+
       track[0] = i;
       track[2]++;
     }
@@ -41,5 +74,8 @@ int main(int argc, char * argv[]) {
   printf("\nCPU execution time: %0.3lfs\n", time_spent);
   printf("Average difference between primes: %0.3lf\n", (double) track[1] / (double) track[2]);
   printf("Max difference between primes: %d\n\n", max);
+  
+  printf("Lucifer Prime Series: %d\n", luciferCounter);
+  printf("All other tri-series: %d\n\n", othersCounter);
 
 }
