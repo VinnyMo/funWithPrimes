f7f37261fae5d5a56781d712a666dc87d5750faf
Vincent Mossman <mossmanv@student-cs.ad.uni.edu>
2016-09-26 22:07:36 -0500

    Cleaned up prime.c

--- prime.c
+++ prime.c
@@ -12,16 +12,26 @@
 
 int main(int argc, char * argv[]) {
 
-  long int test = 1;
-  while (test != 0) {
-    printf("Enter a natural number to test. 0 to exit: ");
-    scanf("%d", &test);
-    if (isPrime(test)) {
-      printf("%d is prime\n", test);
-    }
-    else {
-      printf("%d is not prime\n", test);
-    } // end if (is...
+  if (argc != 2) {
+    printf("Usage: [NAT to test primality]\n");
+    exit(-1);
   }
+
+  long int test;
+  sscanf(argv[1], "%d", &test);
+
+  clock_t begin = clock();
+   
+  if (isPrime(test)) {
+    printf("\n%d is prime\n", test);
+  }
+  else {
+    printf("\n%d is not prime\n", test);
+  }
+
+  clock_t end = clock();
+  double time_spent = (double) (end - begin) / CLOCKS_PER_SEC;
+
+  printf("\nCPU execution time: %0.3fs\n\n", time_spent);
 
 }
