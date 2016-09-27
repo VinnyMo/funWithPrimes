27dfc09cbc90ff64cb29f283c6f035cdfe3a9d00
VinnyMossman <vinny.mossman@gmail.com>
2016-09-26 21:32:37 -0500

    Update prime.c

--- prime.c
+++ prime.c
@@ -12,44 +12,6 @@
 
 int main(int argc, char * argv[]) {
 
-  if (argc != 2) {
-    printf("Usage: %s <followed by [NAT to count to]>\n", argv[0]);
-    exit(-1);
-  }
-  int COUNT_TO, lineCount = 0, i, max = 0;
-  sscanf(argv[1], "%d", &COUNT_TO);
-  int track[4] = {1, 0, 0, 1}; //Previous Prime Position, Total Distance, Total Primes, Line Start Prime
-  clock_t begin = clock();
-  printf("\n");
-  printf("Difference between primes:\n");
-  for (i = 1; i <= COUNT_TO; i++) {
-    if (isPrime(i)){
-      if (lineCount > 19) {
-	printf("    Primes between %d and %d\n", track[3], i);
-	lineCount = 0;
-      }
-      printf("%*d ", 3, i - track[0]); lineCount++;
-      if (lineCount == 1) {
-	track[3] = i;
-      }
-      track[1] = track[1] + (i - track[0]);
-      if (i - track[0] > max) {
-	max = i - track[0];
-      }
-      track[0] = i;
-      track[2]++;
-    }
-  }
-  printf("    Primes between %d and %d\n", track[3], i);
-  clock_t end = clock();
-  double time_spent = (double) (end - begin) / CLOCKS_PER_SEC;
-  printf("\nCPU execution time: %0.3lfs\n", time_spent);
-  printf("Average difference between primes: %0.3lf\n", (double) track[1] / (double) track[2]);
-  printf("Max difference between primes: %d\n\n", max);
-
-
-
-  /*
   long int test = 1;
   while (test != 0) {
     printf("Enter a natural number to test. 0 to exit: ");
@@ -61,44 +23,5 @@ int main(int argc, char * argv[]) {
       printf("%d is not prime\n", test);
     } // end if (is...
   }
-  */
-
-
-  
-  /*
-  if (argc != 3) {
-    printf("Usage: %s <followed by [NAT to count to] followed by [NAT to count by]>\n", argv[0]);
-    exit(-1);
-  }
-  int COUNT_TO, COUNT_BY;
-  sscanf(argv[1], "%d", &COUNT_TO);
-  sscanf(argv[2], "%d", &COUNT_BY);
-  int stats[3] = {COUNT_BY + 1, 0, 0}; //FREQ MIN, FREQ MAX, TOT
-  int i, startCount = 1, primeCount = 0;
-  clock_t begin = clock();
-  printf("\n");
-  for (i = 1; i <= COUNT_TO; i++) {
-    if (isPrime(i)){primeCount++;}
-    if (i % COUNT_BY == 0) {
-      printf("%d primes between %d and %d (inclusive)\n", primeCount, startCount, i);
-      if (primeCount < stats[0]) {
-	stats[0] = primeCount;
-      }
-      if (primeCount > stats[1]) {
-	stats[1] = primeCount;
-      }
-      stats[2] = stats[2] + primeCount;
-      primeCount = 0;
-      startCount = i + 1;
-    }
-  }
-  clock_t end = clock();
-  double time_spent = (double) (end - begin) / CLOCKS_PER_SEC;
-  printf("\nCPU execution time: %0.3lfs\n", time_spent);
-  printf("Lowest frequency per %d: %d\n", COUNT_BY, stats[0]);
-  printf("Highest frequency per %d: %d\n", COUNT_BY, stats[1]);
-  printf("Total Primes under %d: %d\n", COUNT_TO, stats[2]);
-  printf("Average frequency: %0.3lf% (to %d)\n\n", 100 * ((double) stats[2] / (double) COUNT_TO), COUNT_TO);
-*/
 
 }
