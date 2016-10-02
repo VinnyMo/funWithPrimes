9ab5bc320019ec4f42dddaa71f3ba768d38d1487
Vincent Mossman <mossmanv@student-cs.ad.uni.edu>
2016-10-02 18:20:15 -0500

    Parallelization seems to be a bust. Got some nice experience with pthreads though

--- primality.c
+++ primality.c
@@ -4,8 +4,8 @@
 #include <stdio.h>
 
 void * threadPartialTest(void * args);
-bool globalIsPrime = true;
-int numberOfThreads = 1;
+bool globalIsPrime;
+int numberOfThreads = 16;
 long int globalN;
 //pthread_mutex_t updateIsPrimeLock;
 
@@ -15,6 +15,7 @@ long int globalN;
 bool isPrime(long int n) {
 
   globalN = n;
+  globalIsPrime = true;
 
   long i;
   int errorCode;
@@ -38,18 +39,49 @@ bool isPrime(long int n) {
 
 void * threadPartialTest(void * rank) {
 
+  if (globalIsPrime == false) {
+    pthread_exit(0);
+  }
+
   long int n = globalN;
   long int i = 2;
-
-  while (n % i != 0) {
-    if (i > (n / 2)) {
-      i = n;
-      break;
-    }
-    i++;
-  }
-  if (i < n) {
-    globalIsPrime = false;
-  }
  
+  if (n > numberOfThreads * 2) {
+
+    int checkRange = (n / numberOfThreads) / 2;
+
+    if ((long int) rank < (long int) numberOfThreads - 1) {
+      for (i = 2 + ((long int) rank * checkRange); i <= (((long int) rank + 1) * checkRange) + 1; i++) {
+          if (n % i == 0) {
+	  globalIsPrime = false;
+	  break;
+	}
+      }
+    }
+    else {
+      for (i = (numberOfThreads - 1) * checkRange; i <= (n / 2); i++) {
+	if (n % i == 0) {
+	  globalIsPrime = false;
+	  break;
+	}
+      }
+    }
+
+  }
+
+  else {
+
+    while (n % i != 0) {
+      if (i > (n / 2)) {
+	i = n;
+	break;
+      }
+      i++;
+    }
+    if (i < n) {
+      globalIsPrime = false;
+    }
+ 
+  }
+
 }
